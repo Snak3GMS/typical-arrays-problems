ad7586528ee409c666b26fdd4cf241e28fafafd4
vova <vova.shkar74@gmail.com>
2021-01-29 17:32:57 +0300

    fix: Minor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr){
+    return (arr || arguments.length)? arr.length ? Math.min(...arr) : 0 : 0 ;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr){
+    return (arr || arguments.length) ? arr.length ? Math.max(...arr) : 0 : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg (arr){
+    return (arr || arguments.length) ? arr.length ? arr.reduce((a,b)=>a+b) / arr.length : 0 : 0;
 }
+
